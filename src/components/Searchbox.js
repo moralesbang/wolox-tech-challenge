198e1f7d28b4fa1495c15d56f01c62317464b97f
Juan Camilo Morales <camov1209@gmail.com>
2019-05-20 07:42:24 -0500

    refactors codebase

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { setProductQuery } from "../actions";
 import styles from "../assets/styles/Searchbox.module.scss";
